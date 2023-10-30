6547310fe4db6b484c449475366fa4d5573301d5
Muraz <murazaslan@gmail.com>
2023-10-30 13:43:28 +0400

    searcha elaveler

--- js/page-search.js
+++ js/page-search.js
@@ -5,31 +5,31 @@ import { db } from './firebase.js';
 
 const searchForm = document.querySelector('form#search-form')
 const searchInp = document.querySelector('form#search-form [name="search-inp"]')
-const searchBtn = document.querySelector('form#search-form [name="search-btn"]')
 
 searchForm.addEventListener('submit', function (e) {
     e.preventDefault()
     const valueInp = searchInp.value
-    const swiperSlider = document.querySelector('.swiper-slide .book_page')
-    const mySwiper = document.querySelector('.swiper_search')
-    const ajaxBooks = document.querySelector('.ajax_books')
-    
-    
+    const swiperSlide = document.querySelector('.swiper-slide')
+    const swiperWrapper = document.querySelector('.swiper-wrapper')
+
+
 
     onValue(ref(db, `kitablar/`), function (snapshot) {
         const data = Object.entries(snapshot.val())
-        
+        const regEx = new RegExp("^" + valueInp,"gi")
 
         for (let [key, value] of data) {
-            const  result  = (value.name?.trim()?.toLowerCase()).startsWith(valueInp.trim().toLowerCase())
-             
             
-            if (result) {
+         
                 
-                mySwiper.innerHTML = `
-                <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <div class="book_page">
+
+            if (regEx.test(value.name)) {
+                
+
+                swiperSlide.innerHTML = `
+                
+                
+                    <ul class="book_page">
                         <div class="flex">
                             <div class="picture">
                                 <img src="${value.image}" alt="">
@@ -42,20 +42,22 @@ searchForm.addEventListener('submit', function (e) {
                                 </p>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
-                `
-                ajaxBooks.appendChild(mySwiper)
+                    </ul>
                 
+            
+                `
+                swiperWrapper.appendChild(swiperSlide)
+                
+
             }
-           
-           
+            
+
+
         }
+        
+        
     })
-    searchInp.innerHTML = ''
-     
-    
+    searchInp.value = ''
+
+
 })
